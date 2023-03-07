7c2d4ab1ec7dd2cf05cff34cb0293f0fd627ac0e
YoonHan0 <dbsgksdud12@naver.com>
2023-03-07 14:49:03 +0900

    webpack #1 | SASS/SCSS, Image

---
+++ 8.webpack-practice/ex05/src/App.js
@@ -0,0 +1,12 @@
+
+import './assets/scss/App.scss';
+
+function App() {
+    const App = document.createElement('div');
+    App.textContent = "Hello World!";
+    App.className = "Header";
+
+    return App;
+}
+
+export {App}
