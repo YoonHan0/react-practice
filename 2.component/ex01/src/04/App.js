cbc4c3ed5e44014f0ba8504aa7e384de1427a9e6
YoonHan0 <dbsgksdud12@naver.com>
2023-03-08 15:47:11 +0900

    Property Validation

---
+++ 2.component/ex01/src/04/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+
+function App() {
+
+  return (
+    <div id="App">
+      <MyComponent 
+            props01={'문자열입니다아'}
+            props02={100}/>
+    </div>
+  );
+}
+
+export default App;
