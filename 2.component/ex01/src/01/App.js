d36d0518cd019d9bf7a5dad0a23a3aec345e1b95
YoonHan0 <dbsgksdud12@naver.com>
2023-03-08 14:53:30 +0900

    component #ex01 | props, state

---
+++ 2.component/ex01/src/01/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FoodList from './FoodList';
+
+function App() {
+  return (
+    <div id="App">
+      <h1>Food List</h1>
+      <FoodList />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2.component/ex01/src/02/FoodList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import FoodListItem from './FoodListItem'
+
+function FoodList({foods}) {
+  return (
+    <div>
+      <h2>FoodList</h2>
+      <ul>
+          <FoodListItem name='Bread' count='10'/>
+          <FoodListItem name='Egg' count='20'/>
+          <FoodListItem name='Milk' count='5'/>
+      </ul>
+    </div>
+    
+  )
+}
+
+export default FoodList
