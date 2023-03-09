e5bfc55e1a73cd1e409fac0bc674fc61fde0464c
YoonHan0 <dbsgksdud12@naver.com>
2023-03-09 15:14:17 +0900

    kanbanboard #2

--- kanbanboard/frontend/src/App.js
+++ kanbanboard/frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './assets/css/App.css';
+import KanbanBoard from './KanbanBoard';
 
 function App(props) {
-
     return (
-        <div id='App'>
-            <h1>KanbanboaderApp</h1>
+        <div id='App' className={'App'}>            
+           <KanbanBoard />
         </div>
     );
 }
