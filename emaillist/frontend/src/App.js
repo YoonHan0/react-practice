26a2bc7f596cdd5b8db4ed0391ed0f8ff833c330
YoonHan0 <dbsgksdud12@naver.com>
2023-03-09 15:41:18 +0900

    emaillist #init

---
+++ emaillist/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App(props) {
+    return (
+        <div id='App' className={'App'}>            
+           <h1>EmailList</h1>
+        </div>
+    );
+}
+
+export default App;
