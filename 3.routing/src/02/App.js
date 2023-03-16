0975574a8c1893eab1396469f1275943dd90b23e
YoonHan0 <dbsgksdud12@naver.com>
2023-03-16 12:56:57 +0900

    HTML5 history API (popstate)

--- 3.routing/src/02/App.js
+++ 3.routing/src/02/App.js
@@ -6,31 +6,49 @@ import Guestbook from "./component/Guestbook";
 export default function App() {
     const [route, setRoute] = useState({page: '/'});
 
+    useEffect(() => {
+        const handlerPopState = e => {
+            console.log(e.state);
+            setRoute(e.state);
+            // e.state !== null ? setRoute(e.state) : setRoute({page: '/'});
+        }
+        window.addEventListener('popstate', handlerPopState);
+
+        return () => {
+            window.removeEventListener('popstate', handlerPopState);
+        }
+    },[]);
 
 
     const handleLinkClick = (e) => {
         e.preventDefault();
+        console.log(e.target.text); // 태그 안의 text
+        console.log(e.target.href); // 전체 href: http://localhost:9090/
+        console.log(e.target.href.substring(e.target.href.lastIndexOf("/")));   // /
+
+        const url = e.target.href.substring(e.target.href.lastIndexOf("/"));
+        window.history.pushState({page: url}, e.target.text, url);
+
+        setRoute({page: url});
     }
-
-    const router = function () {
-        let component = null;
-        switch (route.page) {
-            case '/':
-                component = <Main/>;
-                break;
-            case '/gallery':
-                component = <Gallery/>;
-                break;
-            case '/guestbook':
-                component = <Guestbook/>;
-                break;
-        }
-
-        return component;
-    };
 
     return (
         <div>
+            {
+                (() => {
+                    switch (route.page) {
+                        case '/':
+                            return <Main/>;
+                        case '/gallery':
+                            return <Gallery/>;
+                        case '/guestbook':
+                            return <Guestbook/>;
+                        default:
+                            null;
+                            break;
+                    }
+                })()
+            }
             <ul>
                 <li><a href={'/'} onClick={handleLinkClick}>[Main]</a></li>
                 <li><a href={'/gallery'} onClick={handleLinkClick}>[Gallery]</a></li>
