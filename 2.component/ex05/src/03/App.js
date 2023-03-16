f1989572d27b1b8edea1f7c8607e81cfe254620c
YoonHan0 <dbsgksdud12@naver.com>
2023-03-16 14:02:47 +0900

    ex05 README.md

--- 2.component/ex05/src/03/App.js
+++ 2.component/ex05/src/03/App.js
@@ -5,13 +5,26 @@ import Clock from './Clock';
 export default class App extends Component {
     constructor() {
         super(...arguments);
+        this.state = {
+            date: new Date(),
+            session: true,
+            handleClockTime() {
+                console.log("\n=============\n" + this);
+                this.setState({date: new Date()}).bind(this);
+            }
+        }
     }
+    
 
     render() {
         return (
             <div className='clock-display'>
                 <h2>ex05 - Component LifeCycle Practice</h2>
-                    <Clock />
+                    <Clock 
+                        hours={this.state.date.getHours()}
+                        minutes={this.state.date.getMinutes()}
+                        seconds={this.state.date.getSeconds()}
+                        session={this.state.session} />
             </div>
         );
     }
