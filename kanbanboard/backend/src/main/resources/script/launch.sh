cb73019af821c8feb4a7b2a84aa08fe1034e8605
YoonHan0 <dbsgksdud12@naver.com>
2023-03-20 13:34:25 +0900

    kanbanboard

--- kanbanboard/backend/src/main/resources/script/launch.sh
+++ kanbanboard/backend/src/main/resources/script/launch.sh
@@ -4,13 +4,13 @@ APPLICATION_NAME=kanbanboard
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 PID=$(ps -ef | grep java | grep $APPLICATION_NAME.jar | awk '{print $2}')
 
-if  [ ! -z "$PID" ] 
+if  [ ! -z "$PID" ]
 then
-	echo "stopping [$APPLICATION_NAME]"
-	kill -9 $PID
-	sleep 10
+    echo "stopping [$APPLICATION_NAME]"
+    kill -9 $PID
+    sleep 10
 fi
 
 echo "starting [$APPLICATION_NAME]"
 cd $SCRIPT_DIR
-nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar >> $SCRIPT_DIR/launch.log &
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar >> $SCRIPT_DIR/launch.log 2>&1 &
